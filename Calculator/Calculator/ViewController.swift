43a08fbb37c7aaa736c000ae1aee6860e670123d
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2021-02-14 13:29:55 +0900

    fix: Fix equalButton, intOrDouble issues
    - equalButton이 제대로 동작하도록 오류 고침
    - intOrDouble function 추가 후 소수끼리 곱하기, 나누기 할 때 발생하는 오류 고침
    - App Icon 추가

--- Calculator/Calculator/ViewController.swift
+++ Calculator/Calculator/ViewController.swift
@@ -15,7 +15,8 @@ class ViewController: UIViewController {
     
     // 과거 상태와 현재 상태 따로 저장
     var pastState = ""
-    var presentState = "" // sum, sub, multi, div, ac, pm, percent
+    var presentState = ""       // sum, sub, multi, div, eq
+    var beforeMultiState = ""   // 곱하기나 나누기 전에 + 인지 - 인지를 저장하는 값
     
     @IBOutlet weak var currentValueLabel: UILabel!
     @IBOutlet weak var sumButton: UIButton!
@@ -35,77 +36,77 @@ class ViewController: UIViewController {
     @IBAction func number0Button(_ sender: Any) {
         valueArr.append("0")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
 
     @IBAction func number1Button(_ sender: Any) {
         valueArr.append("1")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number2Button(_ sender: Any) {
         valueArr.append("2")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number3Button(_ sender: Any) {
         valueArr.append("3")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number4Button(_ sender: Any) {
         valueArr.append("4")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number5Button(_ sender: Any) {
         valueArr.append("5")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number6Button(_ sender: Any) {
         valueArr.append("6")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number7Button(_ sender: Any) {
         valueArr.append("7")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number8Button(_ sender: Any) {
         valueArr.append("8")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func number9Button(_ sender: Any) {
         valueArr.append("9")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func dotButton(_ sender: Any) {
         valueArr.append(".")
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
@@ -115,13 +116,19 @@ class ViewController: UIViewController {
         result = 0.0
         valueArr = del(valueArr)
         inputNum = updateValue(valueArr)
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
     @IBAction func pmButton(_ sender: Any) {
         inputNum = -inputNum
-        currentValueLabel.text = inputNum - Double(Int(inputNum)) == 0 ? String(Int(inputNum)) : String(inputNum)
+        currentValueLabel.text = intOrDouble(inputNum)
+        enableButtons(sumButton, subButton)
+    }
+    
+    @IBAction func percentButton(_ sender: Any) {
+        inputNum = inputNum / 100
+        currentValueLabel.text = intOrDouble(inputNum)
         enableButtons(sumButton, subButton)
     }
     
@@ -136,18 +143,28 @@ class ViewController: UIViewController {
         // 과거 상태의 연산으로 temp 값 설정
         temp = calculateTwoNumbers(state: pastState, num1: temp, num2: inputNum)
         
-        // 과거 상태가 곱하기나 나누기면, result에 temp 값을 더함
+        // 과거 상태가 곱하기나 나누기면, result에 temp 값을 더하거나 뺌
         // 과거 상태가 더하기나 빼기면, result에 inputNum 값을 연산
         if pastState == "multi" || pastState == "div" {
-            result = calculateTwoNumbers(state: "sum", num1: result, num2: temp)
+            if beforeMultiState == "" {
+                result = calculateTwoNumbers(state: pastState, num1: result, num2: inputNum)
+            } else {
+                result = calculateTwoNumbers(state: beforeMultiState, num1: result, num2: temp)
+            }
+//            if beforeMultiState == "sum" {
+//                result = calculateTwoNumbers(state: "sum", num1: result, num2: temp)
+//            } else if beforeMultiState == "sub" {
+//                result = calculateTwoNumbers(state: "sub", num1: result, num2: temp)
+//            }
         } else {        // sum or sub
             result = calculateTwoNumbers(state: pastState, num1: result, num2: inputNum)
         }
         
         // 덧셈 뺄셈의 경우는 result 를 바로 출력한다.
-        currentValueLabel.text = result - Double(Int(result)) == 0 ? String(Int(result)) : String(result)
+        currentValueLabel.text = intOrDouble(result)
         valueArr = del(valueArr)
         sumButton.isUserInteractionEnabled = false
+        beforeMultiState = "sum"
     }
     
     @IBAction func subButton(_ sender: UIButton) {
@@ -158,14 +175,19 @@ class ViewController: UIViewController {
         temp = calculateTwoNumbers(state: pastState, num1: temp, num2: inputNum)
         
         if pastState == "multi" || pastState == "div" {
-            result = calculateTwoNumbers(state: "sum", num1: result, num2: temp)
+            if beforeMultiState == "" {
+                result = calculateTwoNumbers(state: pastState, num1: result, num2: inputNum)
+            } else {
+                result = calculateTwoNumbers(state: beforeMultiState, num1: result, num2: temp)
+            }
         } else {        // sum or sub
             result = calculateTwoNumbers(state: pastState, num1: result, num2: inputNum)
         }
         
-        currentValueLabel.text = result - Double(Int(result)) == 0 ? String(Int(result)) : String(result)
+        currentValueLabel.text = intOrDouble(result)
         valueArr = del(valueArr)
         subButton.isUserInteractionEnabled = false
+        beforeMultiState = "sub"
     }
     
     @IBAction func multiButton(_ sender: UIButton) {
@@ -176,19 +198,17 @@ class ViewController: UIViewController {
         // 과거 상태가 sum 이면 현재 inputNum 을 temp 에 할당
         // 과거 상태가 sub 이면 현재 inputNum 에 - 취한 값을 할당
         // 과거 상태가 곱하기나 나누기면 과거 연산을 수행하여 temp 값에 할당
-        if pastState == "sum" {
-            temp = inputNum
-        } else if pastState == "sub" {
-            temp = -inputNum
-        } else {
+        if pastState == "multi" || pastState == "div" || pastState == "" {
             temp = calculateTwoNumbers(state: pastState, num1: temp, num2: inputNum)
+        } else if pastState == "sum" || pastState == "sub" {
+            temp = inputNum
+        } else {    // "eq"
+            result = inputNum
+            temp = inputNum
         }
         
         // 곱셈 나눗셈의 경우 중간 연산 과정 (temp) 을 출력한다.
-        if temp < 0 {
-            temp = -temp
-        }
-        currentValueLabel.text = temp - Double(Int(temp)) == 0 ? String(Int(temp)) : String(temp)
+        currentValueLabel.text = intOrDouble(temp)
         valueArr = del(valueArr)
         subButton.isUserInteractionEnabled = false
     }
@@ -198,28 +218,37 @@ class ViewController: UIViewController {
         presentState = "div"
         inputNum = Double(currentValueLabel.text ?? "0")!
         
-        if pastState == "sum" {
-            temp = inputNum
-        } else if pastState == "sub" {
-            temp = -inputNum
-        } else {
+        if pastState == "multi" || pastState == "div" || pastState == "" {
             temp = calculateTwoNumbers(state: pastState, num1: temp, num2: inputNum)
+        } else if pastState == "sum" || pastState == "sub" || pastState == "eq" {
+            temp = inputNum
         }
-        if temp < 0 {
-            temp = -temp
-        }
-        currentValueLabel.text = temp - Double(Int(temp)) == 0 ? String(Int(temp)) : String(temp)
+        
+        currentValueLabel.text = intOrDouble(temp)
         valueArr = del(valueArr)
         subButton.isUserInteractionEnabled = false
     }
     
-    // 수정 필요
     @IBAction func equalButton(_ sender: Any) {
-        currentValueLabel.text = String(calculateTwoNumbers(state: presentState, num1: result, num2: inputNum))
+        temp = calculateTwoNumbers(state: presentState, num1: temp, num2: inputNum)
+        
+        if presentState == "multi" || presentState == "div" {
+            if beforeMultiState == "" {
+                result = calculateTwoNumbers(state: presentState, num1: result, num2: inputNum)
+            } else {
+                result = calculateTwoNumbers(state: beforeMultiState, num1: result, num2: temp)
+            }
+        } else {
+            result = calculateTwoNumbers(state: presentState, num1: result, num2: inputNum)
+        }
+        
+        currentValueLabel.text = intOrDouble(result)
+        presentState = "eq"
+        beforeMultiState = ""
     }
-
 }
 
+// Functions
 // calculate two numbers
 func calculateTwoNumbers(state: String, num1: Double, num2: Double) -> Double {
     var result = 0.0
@@ -239,31 +268,15 @@ func calculateTwoNumbers(state: String, num1: Double, num2: Double) -> Double {
     return result
 }
 
-//func calculateANumber(state: String, num: Int) -> Int {
-//    var result = 0
-//
-//    switch state {
-//    case "ac":
-//        result = 0
-//    case "pm":
-//        result = -num
-//    case "percent":
-//        result = num / 100
-//    default:
-//        result = 0
-//    }
-//    return result
-//}
-
-// 출력 값만 (temp, result 등은 그냥 그대로 Double type)
-//func chooseIntDouble(_ value:Double) {
-//    if value - Double(Int(value)) == 0 {
-//        return Int(value)
-//    } else {
-//        return value
-//    }
-//}
-
+// return int type string or double type string
+func intOrDouble(_ value: Double) -> String {
+    let roundedValue = round(value * 1000000000) / 1000000000
+    if roundedValue - Double(Int(roundedValue)) == 0 {
+        return String(Int(roundedValue))
+    } else {
+        return String(roundedValue)
+    }
+}
 
 // update current value
 func updateValue(_ arr: [String]) -> Double {
